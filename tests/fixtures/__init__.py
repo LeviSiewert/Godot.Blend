3ecf8047345ca764d3307d42ed0dee63cca73ebe
[Levi Siewert] <[SiewertLevi@gmail.com]>
2026-06-04 11:52:04 -0400

    cont

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from ...PyGd.structure import *
 
 thisdir = Path(__file__).parent.resolve()
 
@@ -6,6 +7,11 @@ class tscn:
     props  : Path = thisdir / "props/test.tscn"
     test_a : Path = thisdir / "scene/test_a.tscn"
     test_b : Path = thisdir / "scene/test_b.tscn"
+
+class tscn_res:
+    props : GdFile
+    test_a : GdFile
+    test_b : GdFile
 
 class imports:
     svg : Path = thisdir / "import/icon.svg.import"
