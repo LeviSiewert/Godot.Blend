dcdca3f0256efe32504ccb8735320b34d8ac34d5
[Levi Siewert] <[SiewertLevi@gmail.com]>
2026-06-08 16:13:28 -0400

    adding to value parsing

--- GdPy/tests/test_parsing_gd_values.py
+++ GdPy/tests/test_parsing_gd_values.py
@@ -9,6 +9,15 @@ c = Context()
 def _run(key:str, txt:str):
     return gdparser.parse(c,txt,start=key)
 
-def test_rid():
+def test_GdValueResourceID():
     assert(hasattr(gdparser._transformer,"rid"))
-    assert(isinstance(_run("rid", "RID()"), GdValueResourceID)) 
+    assert(isinstance(_run("value", "RID()"), GdValueResourceID)) 
+def test_GdValueExtResource():
+    assert(hasattr(gdparser._transformer,"rid"))
+    assert(isinstance(_run("value", 'ExtResource("")'), GdValueExtResource))
+def test_GdValueNodePath():
+    assert(hasattr(gdparser._transformer,"rid"))
+    assert(isinstance(_run("value", 'NodePath(".")'), GdValueNodePath))
+def test_GdValueSubResource():
+    assert(hasattr(gdparser._transformer,"rid"))
+    assert(isinstance(_run("value", "SubResource()"), GdValueSubResource))
