e22c28968f3da67d878b34b6e0400c3b6b9171d3
[Levi Siewert] <[SiewertLevi@gmail.com]>
2026-06-18 16:21:48 -0400

    cont Property Collection structure

--- GdBl/structure/core/properties.py
+++ GdBl/structure/core/properties.py
@@ -12,19 +12,43 @@ class BlProperty(PropertyGroup):
     val_int : IntProperty() #type:ignore
     val_str : StringProperty() #type:ignore
 
+    val_reflection : StringProperty() #type:ignore
+    ## FUTURE: Allowence for refering back to another property as a nested property
+    ## IE dict[str,list[dict]] ...
+
+class BlPropertyArrayInt(PropertyGroup):
+    items : CollectionProperty(type = IntProperty) #type:ignore
+
+class BlPropertyArrayFloat(PropertyGroup):
+    items : CollectionProperty(type = FloatProperty) #type:ignore
+
+class BlPropertyDictItem(PropertyGroup):
+    key : PointerProperty(type = BlProperty) #type:ignore
+    item : PointerProperty(type = BlProperty) #type:ignore
+
 class BlPropertyDict(PropertyGroup):
     items : CollectionProperty(type = BlProperty) #type:ignore
 
 class BlPropertyArray(PropertyGroup):
+    name : StringProperty() #type:ignore
+    type_a : StringProperty() #type:ignore
+    type_b : StringProperty() #type:ignore
     items : CollectionProperty(type = BlProperty) #type:ignore
 
 class BlPropertyCollection(PropertyGroup):
     items : CollectionProperty(type = BlProperty) #type:ignore
     dict_items : CollectionProperty(type = BlPropertyDict) #type:ignore
     array_items : CollectionProperty(type = BlPropertyArray) #type:ignore
+    array_int_items : CollectionProperty(type = BlPropertyArrayInt) #type:ignore
+    array_float_items : CollectionProperty(type = BlPropertyArrayFloat) #type:ignore
+
+    ## TODO: Add interface
+
 
 _all = (
     BlProperty,
+    BlPropertyArrayInt,
+    BlPropertyArrayFloat,
     BlPropertyDict,
     BlPropertyArray,
     BlPropertyCollection,
