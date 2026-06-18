5b83266450ba7c5c35051383f0ccd40b47728ba4
[Levi Siewert] <[SiewertLevi@gmail.com]>
2026-06-18 08:36:35 -0400

    fix names, placeholder PyToBlProperties

--- GdBl/structure/transformers/references.py
+++ GdBl/structure/transformers/references.py
@@ -1,5 +1,16 @@
 from .core import BlToPy, BlToPyRuleset
 from .core import PyToBl, PyToBlRuleset
 
-gd_to_bl_rulset = BlToPyRuleset(tuple())
-bl_to_gd_rulset = PyToBlRuleset(tuple())
+class PyToBl_Properties(PyToBl):
+    def transform(self, node, c, *args, **kwargs):
+        raise NotImplementedError()
+class BlToPy_Properties(BlToPy):
+    def transform(self, node, c, *args, **kwargs):
+        raise NotImplementedError()
+
+py_to_bl_rulset = BlToPyRuleset(
+    BlToPy_Properties,
+    )
+bl_to_gd_rulset = PyToBlRuleset(
+    BlToPy_Properties,
+    )
--- GdBl/structure/transformers/values.py
+++ GdBl/structure/transformers/values.py
@@ -1,5 +1,5 @@
 from .core import BlToPy, BlToPyRuleset
 from .core import PyToBl, PyToBlRuleset
 
-gd_to_bl_rulset = BlToPyRuleset(tuple())
+py_to_bl_rulset = BlToPyRuleset(tuple())
 bl_to_gd_rulset = PyToBlRuleset(tuple())
