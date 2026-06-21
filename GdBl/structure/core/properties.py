436d2216eb0624e7c0fe30812634865596e36fe7
[Levi Siewert] <[SiewertLevi@gmail.com]>
2026-06-21 18:04:25 -0400

    cont dev

--- GdBl/structure/core/properties.py
+++ GdBl/structure/core/properties.py
@@ -1,5 +1,7 @@
 import bpy
 from typing import Any
+import random
+import string
 
 POINTER_PREFIX : str = "*"
 
@@ -18,7 +20,7 @@ class BlArray(bpy.types.PropertyGroup):
     val_gdtype : bpy.props.StringProperty(default="Variant") #type:ignore
     items : bpy.props.CollectionProperty(type = BlPointerItem) #type:ignore
 
-class BlDictionaryItem():
+class BlDictionaryItem(bpy.types.PropertyGroup):
     key_ptr : bpy.props.PointerProperty(type = BlPointerItem) #type:ignore
     val_ptr : bpy.props.PointerProperty(type = BlPointerItem) #type:ignore
 
@@ -28,7 +30,7 @@ class BlDictionary(bpy.types.PropertyGroup):
     val_gdtype : bpy.props.StringProperty(default="Variant") #type:ignore
     items : bpy.props.CollectionProperty(type = BlDictionaryItem) #type:ignore
 
-class BlFloatVector():
+class BlFloatVector(bpy.types.PropertyGroup):
     _handles : tuple[str] = ("GdValuePackedFloat32Array","GdValuePackedFloat64Array","GdValueVector2","GdValueVector3","GdValueVector4","GdValueRect2","GdValuePlane","GdValueColor","GdValueAABB","GdValueQuaternion","GdValueTransform2D","GdValueBasis","GdValueTransform3D",)
     gdtype : bpy.props.StringProperty() #type:ignore
 
@@ -37,7 +39,7 @@ class BlFloatVector():
     def get_value(self,):
         pass
 
-class BlIntVector():
+class BlIntVector(bpy.types.PropertyGroup):
     _handles : tuple[str] = ("GdValueVector2i","GdValueVector3i","GdValueVector4i","GdValueRect2i","GdValuePackedInt32Array","GdValuePackedInt64Array"),
     gdtype : bpy.props.StringProperty() #type:ignore
 
@@ -46,27 +48,58 @@ class BlIntVector():
     def get_value(self,):
         pass
 
-class BlPrimitive():
-    _handles : tuple[str] = ("GdValueStringName","int","float","bool","None",)
+class BlPrimitive(bpy.types.PropertyGroup):
+    _handles : tuple[str] = ("GdValueStringName","int","float","bool","None","String")
     gdtype : bpy.props.StringProperty() #type:ignore
     val_str : bpy.props.StringProperty() #type:ignore
     val_int : bpy.props.IntProperty() #type:ignore
     val_flt : bpy.props.FloatProperty() #type:ignore
+    val_bool : bpy.props.BoolProperty() #type:ignore
 
-    def set_value(self,):
-        pass
+    def set_value(self, value:Any):
+        match self.gdtype:
+            case "String":
+                self.val_str = value
+            case "GdValueStringName":
+                self.val_str = value
+            case "int":
+                self.val_int = value
+            case "float":
+                self.val_flt = value
+            case "bool":
+                self.val_bool = value
+            case "None":
+                pass
+            case _:
+                raise KeyError(self.gdtype, value)
+
     def get_value(self,):
-        pass
+        match self.gdtype:
+            case "String":
+                return self.val_str
+            case "GdValueStringName":
+                return self.val_str
+            case "int":
+                return self.val_int
+            case "float":
+                return self.val_flt
+            case "bool":
+                return self.val_bool
+            case "None":
+                return None
+        
 
 def _invert_dict_tuple(di:dict[str,tuple[str]])->dict[str,str]:
     res = {}
-    for v,kl in di:
+    for v,kl in di.items():
         for k in kl:
             res[k] = v
     return res
 
-class BlPropertyCollection():
-    properties : bpy.prop.CollectionProperty(type = BlPropertyItem) #type:ignore
+class BlPropertyCollection(bpy.types.PropertyGroup):
+    ''' Class that encompasses bins and properties, where any element can hold and query pointers held by the root '''
+
+    properties : bpy.props.CollectionProperty(type = BlPropertyItem) #type:ignore
 
     bin_primitives : bpy.props.CollectionProperty(type = BlPrimitive) #type:ignore
     bin_int_vectors : bpy.props.CollectionProperty(type = BlIntVector) #type:ignore
@@ -89,13 +122,15 @@ class BlPropertyCollection():
             yield getattr(self,k)
 
     def get(self, key:str, default=_UNSET, return_ptr=False):
+        if key.startswith(POINTER_PREFIX):
+            return self.fetch_pointer_data(key, default)
         if res:=self.properties.get(key, None):
-            return res
+            if return_ptr:
+                return res
+            return self.fetch_pointer_data(res.value)
         if default is _UNSET:
             raise KeyError(key)
-        if return_ptr:
-            return res
-        return self.fetch_pointer_data(res.value)
+        return default
     
     def fetch_pointer_data(self, ptr:str, default=_UNSET):
         for c in self._yield_bins():
@@ -105,8 +140,10 @@ class BlPropertyCollection():
             raise KeyError(ptr)
         return default
 
-    def fetch_pointer_tree(self, ptr:str, chain:list[str])->list:
+    def fetch_pointer_tree(self, ptr:str, chain:list[str]=None)->list:
         ''' Fetch a list of references by structure traversal '''
+        if chain is None:
+            chain = []
         if ptr in chain:
             return tuple()
         res = []
@@ -121,30 +158,48 @@ class BlPropertyCollection():
                     res.extend(self.fetch_pointer_tree(item.val_ptr.value, chain))
         return res
     
-    def remove(self, key:str):
-        prop = self.properties[key]
-        affected = self.fetch_pointer_tree(prop.value)
+    def remove_property(self, key:str):
+        ptr = self.properties[key]
+        affected = self.fetch_pointer_tree(ptr.value)
         for c in self._yield_bins():
-            c_keys = c.keys()
+            c : bpy.types.CollectionProperty
+            c_keys = tuple(c.keys())
             for ptr in affected:
-                if ptr in c_keys:
-                    c.remove(ptr)
+                if ptr in c_keys:   
+                    c.remove(c_keys.index(ptr))
                     affected.remove(ptr)
+        _keys = tuple(self.properties.keys())
+        self.properties.remove(_keys.index(key))
     
-    def new(self, gdtype:str, key:str, value:str, *args, **kwargs)->tuple[Any,BlPropertyItem]:
+    def new(self, gdtype:str, key:str, *args, **kwargs)->tuple[Any,BlPropertyItem]:
         col = getattr(self,self.BIN_MAP[gdtype])
         p_str = self._generate_pointer()
 
-        obj = col.new()
+        obj = col.add()
         obj.name = p_str 
         obj.gdtype = gdtype
         obj.set_value(*args, **kwargs)
 
-        ptr = self.properties.new()
+        ptr = self.properties.add()
         ptr.name = key
         ptr.value = p_str
         
         return obj, ptr
+    
+    def _generate_pointer(self,)->str:
+        _all = []
+        for c in self._yield_bins():
+            _all.extend(c.keys())
+        r = POINTER_PREFIX+"".join(random.sample(string.ascii_letters, 9))
+        while r in _all:
+            r = POINTER_PREFIX+"".join(random.sample(string.ascii_letters, 9))
+        return r 
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __delitem__(self, key):
+        self.remove_property(key)
 
 _all = (
     BlPointerItem,
--- GdBl/tests/test_property_collection.py
+++ GdBl/tests/test_property_collection.py
@@ -1,3 +1,23 @@
+import bpy
 from .utils import BlenderPytestAttr
-from ..structure.core.properties import BlPropertyCollection, BlProperty, BlPropertyArray, BlPropertyDict, BlPropertyArrayFloat,BlPropertyArrayInt,BlPropertyArrayVector
+from ..structure.core.properties import BlPropertyCollection
 
+class TestPropertyCollection(BlenderPytestAttr):
+    attr_value = bpy.props.PointerProperty(type = BlPropertyCollection) #type:ignore
+
+    def _basic(self, gdtype:str, propname:str, value:Any ):
+        propcol : BlPropertyCollection = self.get_attr()
+
+        obj,ptr = propcol.new(gdtype, propname, value)
+                
+        assert(ptr.name == propname)
+        assert(ptr == propcol.get(propname, return_ptr=True))
+                
+        assert(propcol.get(ptr.value) == obj)
+
+        assert(obj.get_value() == value)
+        
+        del propcol[ptr.name]
+
+        for c in propcol._yield_bins():
+            assert(len(c) == 0)
