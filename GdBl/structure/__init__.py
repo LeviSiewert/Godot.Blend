6939c7caaad9e66c01ab18fe981c984733eb83c0
[Levi Siewert] <[SiewertLevi@gmail.com]>
2026-06-28 17:08:43 -0400

    c

--- GdBl/structure/__init__.py
+++ GdBl/structure/__init__.py
@@ -17,7 +17,7 @@ _all = (
 def register():
     for c in _all:
         bpy.utils.register_class(c)
-    bpy.types.Collection.gd = bpy.props.PointerProperty(type=resources.BlTscnOnCollection)
+    # bpy.types.Collection.gd = bpy.props.PointerProperty(type=resources.BlTscnOnCollection)
     # bpy.types.Text.gd = bpy.props.PointerProperty(type=file.BlFileCollection)
     # bpy.types.Object.gd = bpy.props.PointerProperty(type=sub_resources.BlNodeResource)
 
@@ -25,5 +25,5 @@ def unregister():
     for c in reversed(_all):
         bpy.utils.unregister_class(c)
     # del bpy.types.Text.gd
-    del bpy.types.Collection.gd
+    # del bpy.types.Collection.gd
     # del bpy.types.Object.gd
--- GdBl/structure/core/types.py
+++ GdBl/structure/core/types.py
@@ -1,22 +1,38 @@
 import bpy 
 
-class _BlReference(bpy.types.PropertyGroup):
-    pass
+# class _BlReference(bpy.types.PropertyGroup):
+#     pass
 
-class _BlSubResource(bpy.types.PropertyGroup):
+class _BlSubResource(bpy.types.PropertyGroup): 
     pass
 
 class _BlSubResourceCollection(bpy.types.PropertyGroup):
-    items : bpy.types.PropertyCollection
+    data : bpy.types.PropertyCollection
 
 class _BlResource(bpy.types.PropertyGroup):
+    ''' Place on all objects that require a unique disc representation (Tscn, Gltf, ~Textures) 
+    required standard is object.gd
+    in cases where an object represents multiple files, use .gd.{filetype}
+    '''
+    is_enabled : bpy.props.BoolProperty() #type:ignore
     filepath : bpy.props.StringProperty() #type:ignore
     uid : bpy.props.StringProperty() #type:ignore
-    cached_dependencies : list[str]
+
+class _BlResourceSettings(bpy.types.PropertyGroup):
+    ''' Settings container, utilize for all _BlResources subtypes that have import/export settings that should be stored on the local object (Tscn, Gltf, Textures, Ect.) 
+    required standard is .gd_settings
+    in cases where an object represents multiple files, use .gd_settings.{filetype}
+    '''
 
 class _BlResourceCollection(bpy.types.PropertyGroup):
-    items : bpy.types.PropertyCollection
+    ''' Utilize when clumping specific types of _BlResources '''
+    data : bpy.types.PropertyCollection
 
-class _BlProject():
+class _BlProjectData(bpy.types.PropertyGroup):
+    ''' Project data accessor, utilize to point to other shit'''
+    pass
+
+class _BlProject(bpy.types.PropertyGroup):
+    ''' Preferences project representation '''
     name : bpy.props.StringProperty(name="Name") #type:ignore
     filepath : bpy.props.StringProperty(name="project_dir", subtype="DIR_PATH") #type:ignore
