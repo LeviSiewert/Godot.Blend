f4638270e00ecd0ca2a185417dca671d88d0525b
[Levi Siewert] <[SiewertLevi@gmail.com]>
2026-07-02 22:16:02 -0400

    cont dev

--- GdPySimpler/tests/transformers/tscn/test_structure.py
+++ GdPySimpler/tests/transformers/tscn/test_structure.py
@@ -80,43 +80,126 @@ class _StructureTest[T:Type]():
 
 
 class Test_ResourceSettings(_StructureTest):
-    ...
+    _parser_key = "resource_settings"
+    _type = ResourceSettings
 
 class Test_ResourceTres(_StructureTest):
-    ...
+    _parser_key = "resource_tres"
+    _type = ResourceTres
 
 class Test_ResourceScene(_StructureTest):
-    ...
+    _parser_key = "resource_scene"
+    _type = ResourceScene
 
 class Test_SubResource(_StructureTest):
-    ...
+    _parser_key = "sub_resource"
+    _type = SubResource
 
 class Test_SubResourceCollection(_StructureTest):
-    ...
+    _parser_key = "sub_resources"
+    _type = SubResourceCollection
 
 class Test_Node(_StructureTest):
-    ...
+    _parser_key = "node_resource"
+    _type = Node
 
 class Test_NodeCollection(_StructureTest):
-    ...
+    _parser_key = "node_resources"
+    _type = NodeCollection
 
 class Test_Category(_StructureTest):
-    ...
+    _parser_key = "cat_resource"
+    _type = Category
 
 class Test_CategoryCollection(_StructureTest):
-    ...
+    _parser_key = "cat_resources"
+    _type = CategoryCollection
 
 class Test_ExtReference(_StructureTest):
-    ...
+    _parser_key = "ext_reference"
+    _type = ExtReference
 
-class Test_ExtReferenceCollection(_StructureTest):
-    ...
+    def setup_class(self,):
+        raise NotImplementedError()
+        self._project = Project.construct()
+
+    def make_gdtopy_context(self):
+        c = GdToPyContext()
+        return c
+
+    def make_pytogd_context(self):
+        c = PyToGdContext()
+        return c
+        
+    def data(self):
+        txt = '''[ext_resource type="PackedScene" uid="uid://bvshg3b45tq5b" path="res://assets/blender.glb.tscn" id="1_0xe7n"] '''
+        res = ExtReference(type="PackedScene", uid=self._project.resources.get("uid://bvshg3b45tq5b"), path=self._project.files.get("res://assets/blender.glb.tscn"), id="1_0xe7n")
+        yield txt,res
+
+        txt = '''[ext_resource type="PackedScene" uid="uid://cocfi2vsn5qt2" path="res://assets/blender.glb" id="1_w5rjj"] '''
+        res = ExtReference(type="PackedScene", uid="uid://cocfi2vsn5qt2", path="res://assets/blender.glb", id="1_w5rjj")
+        yield txt,res
+        
+        txt = '''[ext_resource type="Script" uid="uid://cr1tpol7u62kd" path="res://assets/script.gd" id="3_7d4mc"] '''
+        res = ExtReference(type="Script", uid="uid://cr1tpol7u62kd", path="res://assets/script.gd", id="3_7d4mc")
+        yield txt,res
+    
+
+class Test_ExtReferenceCollection(Test_ExtReference):
+    _type = ExtReferenceCollection
+    _parser_key = "ext_references"
+
+
+    def data():
+        txt = '''
+[ext_resource type="PackedScene" uid="uid://bvshg3b45tq5b" path="res://assets/blender.glb.tscn" id="1_0xe7n"]
+[ext_resource type="PackedScene" uid="uid://cocfi2vsn5qt2" path="res://assets/blender.glb" id="1_w5rjj"]
+[ext_resource type="Script" uid="uid://cr1tpol7u62kd" path="res://assets/script.gd" id="3_7d4mc"]
+'''     
+        res = ExtReferenceCollection(
+            ExtReference(type="PackedScene", uid="uid://bvshg3b45tq5b", path="res://assets/blender.glb.tscn" id="1_0xe7n"),
+            ExtReference(type="PackedScene", uid="uid://cocfi2vsn5qt2", path="res://assets/blender.glb" id="1_w5rjj"),
+            ExtReference(type="Script", uid="uid://cr1tpol7u62kd", path="res://assets/script.gd" id="3_7d4mc"),
+        )
+        yield txt, res
 
 class Test_EditFlag(_StructureTest):
-    ...
+    _type = EditFlag
+    _parser_key = "edit_flag"
+    def data(self,):
+        txt = ''' [editable path="GlbInhheritedEditable"] '''
+        res = EditFlag("GlbInhheritedEditable")
+        yield txt, res
+        
+        txt = ''' [editable path="GlbEditable"] '''
+        res = EditFlag("GlbEditable")
+        yield txt, res
+        
+        txt = ''' [editable path="GlbEditableAddScript"] '''
+        res = EditFlag("GlbEditableAddScript")
+        yield txt, res
+        
+        txt = ''' [editable path="GlbEditableEditProp"] ''' 
+        res = EditFlag("GlbEditableEditProp")
+        yield txt, res
 
 class Test_EditFlagCollection(_StructureTest):
-    ...
+    _type = EditFlagCollection
+    _parser_key = "edit_flag"
+    def data(self,):
+        txt = '''
+[editable path="GlbInhheritedEditable"]
+[editable path="GlbEditable"]
+[editable path="GlbEditableAddScript"]
+[editable path="GlbEditableEditProp"]
+''' 
+        res = EditFlagCollection(
+            EditFlag("GlbInhheritedEditable").
+            EditFlag("GlbEditable").
+            EditFlag("GlbEditableAddScript").
+            EditFlag("GlbEditableEditProp").
+        )
+        yield txt, res
 
 class Test_GdType(_StructureTest):
     ...
@@ -125,6 +208,7 @@ class Test_GdTypeValueSet(_StructureTest):
     ...
 
 class Test_Signal(_StructureTest):
+    _type = Signal
     def data(self,):
         
         txt = '''[connection signal="body_entered" from="." to="." method="_on_door_body_entered"]'''     
