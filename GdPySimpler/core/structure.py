34c416f8330e57d91d27ae98929f95c48388fdb0
[Levi Siewert] <[SiewertLevi@gmail.com]>
2026-07-06 09:00:49 -0400

    moving subresources to seperate file

--- GdPySimpler/core/structure.py
+++ GdPySimpler/core/structure.py
@@ -17,7 +17,7 @@ class StructContext(_StructContext):
     project : Project | None
     file: _File | None
     resource: _Resource | None
-    sub_resource: SubResource | None
+    sub_resource: Any | None
 
 class GdValue():
     ...
@@ -106,29 +106,8 @@ class ResourceSettings(_Resource):
         self.properties = PropertyCollection()
         self.cat_resources = CategoryCollection()
 
-class ResourceTres(_Resource):
-    type : GdType|None|str
-    format : int
-    script : str #TEMP! resolve to from typing eventually w/a
-    script_class : str #TEMP! resolve to from typing eventually w/a
-    
-    properties : PropertyCollection
 
-    ext_references : ExtReferenceCollection # Contextual re-mapping, req stability for diffing, export should trim based on ref count.
-    sub_resources : SubResourceCollection
-
-    def __setup__(self):
-        self.properties = PropertyCollection()
-        self.ext_references = ExtReferenceCollection()
-        self.sub_resources = SubResourceCollection()
-        return self
-    
-    def __init__(self, type, format, uid, script_class:str=None):
-        self.type = type
-        self.script_class = script_class
-        super().__init__(format=format, uid=uid)
-
-class ResourceScript(ResourceTres):
+class ResourceScript(_Resource):
     pass
 
 
@@ -256,7 +235,6 @@ class ExtReferenceCollection(Collection):
         return "id"
 
 
-
 class EditFlag():
     path : str
     def __init__(self, path):
@@ -265,48 +243,6 @@ class EditFlag():
 class EditFlagCollection(Collection):
     unique_keys = ("path",)
     _type = EditFlag
-
-
-class SubResource():
-    context : StructContext
-    owner : _Resource|None = None
-    unique_id : Key[str]
-    type : GdType|None = None
-    
-    instance : _Resource
-    instance_editable : bool = False
-
-    overlay : SubResource|None = None
-    overlay_is_thin : bool = False
-    
-    properties : PropertyCollection
-
-    def __setup__(self):
-        self.context = StructContext(sub_resource=self)
-        self.unique_id = Key(self, "unique_id", None)
-        self.properties = PropertyCollection()
-        return self
-
-    def __init__(self, /, owner:_Resource|None=None, overlay:SubResource=None, type:Type=None, instance:_Resource=None, instance_editable:bool=False, unique_id:Any=None):
-        self.__setup__()
-        if not (unique_id is None):
-            self.unique_id.set(unique_id)
-
-        self.set_owner(owner)
-        self.set_type(type)
-        
-        if instance:
-            assert(overlay is None)
-            self.set_overlay(instance.data.root)
-            self.instance = instance
-            self.instance_editable = instance_editable
-        elif overlay:
-            self.set_overlay(overlay)
-
-class SubResourceCollection(Collection):
-    unique_keys = ("unique_id",)
-    _type = SubResource
-
 
 
 class Category():
@@ -328,28 +264,6 @@ class CategoryCollection(Collection):
     _type = Category
 
 
-# class _ContextualRef(Reference, GdValue):
-    # context : StructContext
-    # def _on_context_updated(self, attr:str, value:Any|None):
-    #     pass
-
-class SubResourceRef(Reference, GdValue): 
-    key_categories = ("id",)
-    _type = SubResource
-
-    def __setup__(self):
-        super().__setup__()
-        self.context = StructContext()
-        self.context.callback("resource",self._on_context_updated)
-
-    def _on_context_updated(self, value:Any):
-        if value is None:
-            self.set_collection(None)
-        else:
-            value : ResourceTres
-            self.set_collection(value.sub_resources)
-
-
 class ExtResourceRef(Reference, GdValue): 
     ''' Routed reference ID '''
     key_categories = ("id",)
@@ -364,7 +278,7 @@ class ExtResourceRef(Reference, GdValue):
         if value is None:
             self.set_collection(None)
         else:
-            value : ResourceTres
+            value : _Resource
             self.set_collection(value.ext_resources)
 
 class RID(Reference, GdValue):
---
+++ GdPySimpler/core/subresources.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+from typing import Type, Any
+
+from .structure import _Resource, StructContext, GdType, GdValue, ExtReferenceCollection
+from .collections import Key, Reference, Collection
+from .property_collection import PropertyCollection
+
+
+class SubResource():
+    context : StructContext
+    owner : _Resource|None = None
+    unique_id : Key[str]
+    type : GdType|None = None
+    
+    instance : _Resource
+    instance_editable : bool = False
+
+    overlay : SubResource|None = None
+    overlay_is_thin : bool = False
+    
+    properties : PropertyCollection
+
+    def __setup__(self):
+        self.context = StructContext(sub_resource=self)
+        self.unique_id = Key(self, "unique_id", None)
+        self.properties = PropertyCollection()
+        return self
+
+    def __init__(self, /, owner:_Resource|None=None, overlay:SubResource=None, type:Type=None, instance:_Resource=None, instance_editable:bool=False, unique_id:Any=None):
+        self.__setup__()
+        if not (unique_id is None):
+            self.unique_id.set(unique_id)
+
+        self.set_owner(owner)
+        self.set_type(type)
+        
+        if instance:
+            assert(overlay is None)
+            self.set_overlay(instance.data.root)
+            self.instance = instance
+            self.instance_editable = instance_editable
+        elif overlay:
+            self.set_overlay(overlay)
+
+
+class SubResourceCollection(Collection):
+    unique_keys = ("unique_id",)
+    _type = SubResource
+
+
+class SubResourceRef(Reference, GdValue): 
+    key_categories = ("id",)
+    _type = SubResource
+
+    def __setup__(self):
+        super().__setup__()
+        self.context = StructContext()
+        self.context.callback("resource",self._on_context_updated)
+
+    def _on_context_updated(self, value:Any):
+        if value is None:
+            self.set_collection(None)
+        else:
+            value : ResourceTres
+            self.set_collection(value.sub_resources)
+
+
+class ResourceTres(_Resource):
+    type : GdType|None|str
+    format : int
+    script : str #TEMP! resolve to from typing eventually w/a
+    script_class : str #TEMP! resolve to from typing eventually w/a
+    
+    properties : PropertyCollection
+
+    ext_references : ExtReferenceCollection # Contextual re-mapping, req stability for diffing, export should trim based on ref count.
+    sub_resources : SubResourceCollection
+
+    def __setup__(self):
+        self.properties = PropertyCollection()
+        self.ext_references = ExtReferenceCollection()
+        self.sub_resources = SubResourceCollection()
+        return self
+    
+    def __init__(self, type, format, uid, script_class:str=None):
+        self.type = type
+        self.script_class = script_class
+        super().__init__(format=format, uid=uid)
