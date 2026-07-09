bbc3eb28360b47ef48e33e12079d0da350456a75
[Levi Siewert] <[SiewertLevi@gmail.com]>
2026-07-09 12:30:07 -0400

    re-expanding to subtype allowences

--- GdBl/core/property_collection.py
+++ GdBl/core/property_collection.py
@@ -8,7 +8,7 @@ from .primitives.pointer_collection import (
 )
 
 class GdPropertyCollection(_PointerCollection):    
-    _bins = ("bin_array","bin_dict","bin_vector","bin_primitive", "bin_reference")
+    _bins = ("bin_array", "bin_dict", "bin_vector", "bin_primitive", "bin_reference")
 
     bin_dict : bpy.props.CollectionProperty(type=GdDictionary) #type:ignore
     bin_array : bpy.props.CollectionProperty(type=GdArray) #type:ignore
@@ -17,21 +17,131 @@ class GdPropertyCollection(_PointerCollection):
     bin_vector : bpy.props.CollectionProperty(type = GdVector) #type:ignore
     bin_reference : bpy.props.CollectionProperty(type= GdReference) #type:ignore
 
+    def _bin_val_matcher(self, val):
+        ty = val.__class__
+        tyname = ty.__name__
+        for b, name in ((getattr(self,x),x) for x in self._bins):
+            if (tyname in b._subtypes) or (ty in b._subtypes):
+                return name 
+        return super()._bin_val_matcher(val)
+    
+    ##TODO: Draw
+
+
 class GdDictionary(_BlPointerDictionary):
+    _subtypes = ("Dictionary",)
+    subtype = "Dictionary"
     typing : bpy.props.StringProperty() #type:ignore
+
+    ##TODO: Draw
+
 
 class GdArray(_BlPointerArray):
+    _subtypes = ("Array",)
+    subtype = "Array"
     typing : bpy.props.StringProperty() #type:ignore
 
-class GdPrimitive(bpy.types.PropertyGroup):
-    type : bpy.props.StringProperty() #type:ignore
-    
-class GdVector(bpy.types.PropertyGroup):
-    type : bpy.props.StringProperty() #type:ignore
-    
+    ##TODO: Draw
+
+
 class GdReference(bpy.types.PropertyGroup):
+    _subtypes = ("ExtResourceRef", "SubResourceRef", "RID", "ResourceRef")
     typing : bpy.props.StringProperty() #type:ignore
-    type : bpy.props.StringProperty() #type:ignore
+    subtype : bpy.props.StringProperty() #type:ignore
+
+    val_str : bpy.props.StringProperty() #type:ignore
+
+    def _set_subtype_fr_val(self,val):
+        subtype = val.__class__.__name__
+        if subtype in self._subtypes:
+            self.subtype = subtype
+
+    @property
+    def value(self, v):
+        self._set_subtype_fr_val(v)
+        self.val_str = v
+    @value.getter
+    def value(self,):
+        return self.val_str
+
+    def draw(self, layout):
+        if self.subtype:
+            layout.prop(self, self.subtype)
+        else:
+            pass
+
+
+class GdPrimitive(bpy.types.PropertyGroup):
+    _subtypes = ("StringName", "str", "int", "float", "bool", "None")
+    subtype : bpy.props.StringProperty() #type:ignore
+
+    val_stringname : bpy.props.StringProperty() #type:ignore
+    val_str : bpy.props.StringProperty() #type:ignore
+    val_int : bpy.props.IntProperty() #type:ignore
+    val_float : bpy.props.FloatProperty() #type:ignore
+    val_bool : bpy.props.BoolProperty() #type:ignore
+    val_none = None
+
+    def _set_subtype_fr_val(self,val):
+        subtype = val.__class__.__name__
+        if subtype in self._subtypes:
+            self.subtype = subtype
+
+    @property
+    def value(self, v):
+        self._set_subtype_fr_val(v)
+        if v is None: return
+        setattr(self, "val_"+self.subtype.lower(), v)
+    @value.getter
+    def value(self,):
+        return getattr(self, "val_"+self.subtype.lower())
+
+    def draw(self, layout):
+        if self.subtype:
+            layout.prop(self, self.subtype)
+        else:
+            pass
+
+class GdVector(bpy.types.PropertyGroup):
+    _subtypes = ("Vector2", "Vector3", "Vector4", "Rect2", "Plane", "Color", "AABB", "Quaternion", "Basis", "Transform2D", "Transform3D", "Vector2i", "Vector3i", "Vector4i", "Rect2i")
+    subtype : bpy.props.StringProperty() #type:ignore
+    
+    vector2 : bpy.props.FloatVectorProperty(size = 2) #type:ignore
+    vector3 : bpy.props.FloatVectorProperty(size = 3) #type:ignore
+    vector4 : bpy.props.FloatVectorProperty(size = 4) #type:ignore
+    rect2 : bpy.props.FloatVectorProperty(size = 4) #type:ignore
+    plane : bpy.props.FloatVectorProperty(size = 4) #type:ignore
+    color : bpy.props.FloatVectorProperty(size = 4, subtype="COLOR") #type:ignore
+    aabb : bpy.props.FloatVectorProperty(size = 6) #type:ignore
+    quaternion : bpy.props.FloatVectorProperty(size = 4, subtype="QUATERNION") #type:ignore
+    basis : bpy.props.FloatVectorProperty(size = 9, subtype="MATRIX") #type:ignore
+    
+    transform2d : bpy.props.FloatVectorProperty(size = 6) #type:ignore
+    transform3d : bpy.props.FloatVectorProperty(size = 12) #type:ignore
+    
+    vector2i : bpy.props.IntVectorProperty(size=2) #type:ignore
+    vector3i : bpy.props.IntVectorProperty(size=3) #type:ignore
+    vector4i : bpy.props.IntVectorProperty(size=4) #type:ignore
+    rect2i : bpy.props.IntVectorProperty(size=4) #type:ignore
+
+    def _set_subtype_fr_val(self,val):
+        subtype = val.__class__.__name__
+        if subtype in self._subtypes:
+            self.subtype = subtype
+
+    @property
+    def value(self, v):
+        self._set_subtype_fr_val(v)
+        setattr(self, self.subtype.lower(), v)
+    @value.getter
+    def value(self,):
+        return getattr(self, self.subtype.lower())
+
+    def draw(self, layout):
+        if self.subtype:
+            layout.prop(self, self.subtype)
+        else:
+            pass
 
 
 from .primitives.pointer_collection import (
