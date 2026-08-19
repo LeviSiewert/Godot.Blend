f0221f86deded2b526ac235ca8550d74e40a1e7b
[Levi Siewert] <[SiewertLevi@gmail.com]>
2026-08-19 19:23:28 -0400

    c dev, adding base structure

--- GdPy_Simpler4/structure.py
+++ GdPy_Simpler4/structure.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from collection import UserDict
 
 from fsspec import AbstractFileSystem
+from typing import Iterable
 
 from .signals import Signal
 from .context import Context
@@ -56,11 +57,19 @@ class ExtResource():
 class File():
     context : Context
 
+    path : CollectionKey[str]
+
     _resource : StructReference[Resource]
     resource = StructReferenceProperty("_resource", RefType.RID)
 
     def __setup__(self):
         self.context = Context()
+        self.path = CollectionKey(src=self)
+
+    def __init__(self, path:str, resource:str|Resource|None=None):
+        self.__setup__()
+        self.path = path
+        self.resource = resource
 
 class Resource():
     context : Context
@@ -76,6 +85,13 @@ class Resource():
     id : CollectionKey[str]
     gdtype : GdDefType
     properties : Properties
+
+    def __init__(self, id:str|None=None, uid:str|None=None, file:File|None=None, properties:Iterable|dict=tuple()):
+        self.__setup__()
+        self.id.key = id
+        if uid or file:
+            self.__setup_file__(uid=uid, file=file)
+        self.properties.extend(properties)
 
     def __setup__(self):
         self.context = Context(resource = self)
@@ -95,5 +111,22 @@ class Resource():
     def is_subresource(self)->bool:
         return (self.uid._key is None)
     
+class Node(Resource):
 
-    
+    nodes : Collection[int, Node]
+
+    name : CollectionKey[str]
+    children : Collection[str, Node]
+
+    def __setup__(self):
+        super().__setup__()
+        self.name = CollectionKey(src = self)
+        self.children = Collection(key_attr="name", context=self.context, key_resolve_incriment=True)
+
+    def __setup_file__(self, uid = None, file = None):
+        self.nodes = Collection(key_attr="id", key_is_string=False)
+        return super().__setup_file__(uid, file)
+
+    def __init__(self, name=None, id = None, uid = None, file = None, properties = tuple()):
+        self.name.key = name
+        super().__init__(id, uid, file, properties)
