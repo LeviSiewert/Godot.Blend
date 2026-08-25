e54c9980f656a7f8aedf33b7c798a46ac694d136
[Levi Siewert] <[SiewertLevi@gmail.com]>
2026-08-24 21:33:25 -0400

    c dev, framing contents of definitions

--- GdPy_Simpler4/core/defininitions.py
+++ GdPy_Simpler4/core/defininitions.py
@@ -1,5 +1,23 @@
-class GdDefType:...
-class GdDefProperty:...
-class GdDefSignal:...
+from __future__ import annotations
 
-class GdDefValue:...
+from typing import Type, Any
+from .context import Context
+
+class GdDefValue:
+    base : Type = None
+    contents_a : Type|Any = Any
+    contents_b : Type|Any = Any
+
+class GdDefProperty:
+    context : Context
+    name : str
+    value : GdDefValue|Any = Any
+    ...
+
+class GdDefSignal:
+    context : Context
+    ...
+
+class GdDefType:
+    extends : GdDefType|None = None
+    context : Context
