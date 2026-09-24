69b485af8353b9dc12126afb3f2c484b90bfe52c
[Levi Siewert] <[SiewertLevi@gmail.com]>
2026-09-24 16:11:06 -0400

    c dev

--- GdPy/transformers/gd_to_py/_core.py
+++ GdPy/transformers/gd_to_py/_core.py
@@ -5,7 +5,6 @@ from typing import Iterable
 
 from . import (
     values,
-    promises,
     defintions,
     structure,
 )
@@ -13,7 +12,6 @@ from . import (
 def make_gd_to_py[TS:GdToPy_TransformerSet[GdToPy_Transformer]](insert:Iterable[TS]=tuple())->Session[GdToPy_TransformerSet[GdToPy_Transformer]|TS]:
     return GdToPy_Session([
         values.gd_to_py,
-        promises.gd_to_py,
         defintions.gd_to_py,
         structure.gd_to_py,
         *insert
@@ -22,7 +20,6 @@ def make_gd_to_py[TS:GdToPy_TransformerSet[GdToPy_Transformer]](insert:Iterable[
 def make_py_to_gd[TS:PyToGd_TransformerSet[PyToGd_Transformer]](insert:Iterable[TS]=tuple())->Session[PyToGd_TransformerSet[GdToPy_Transformer]|TS]:
     return PyToGd_Session([
         values.py_to_gd,
-        promises.py_to_gd,
         defintions.py_to_gd,
         structure.py_to_gd,
         *insert
